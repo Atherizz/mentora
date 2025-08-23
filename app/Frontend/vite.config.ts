435f0b61ab7da72339507e5865307bd043217663
Atherizz <saveroathallah86@gmail.com>
2025-08-23 21:06:22 +0700

    another revision

--- app/Frontend/vite.config.ts
+++ app/Frontend/vite.config.ts
@@ -21,4 +21,12 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  server: {
+    host: "0.0.0.0", 
+    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
+  },
 });
