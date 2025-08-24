4dad315fbc7392b84e2f1ad3fbe02e3b4e27e0e4
Atherizz <saveroathallah86@gmail.com>
2025-08-24 09:29:53 +0700

    redeploy

--- app/Backend/config/config.js
+++ app/Backend/config/config.js
@@ -20,7 +20,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: 'localhost',
+    // host: 'localhost',
     dialect: 'mysql',
     dialectOptions: {
       socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
